eabaeeb3a7ae56da8ee7890ff3197366e1197289
Kyra <kyra.dev.07@gmail.com>
2024-03-10 16:10:10 -0500

    Se eliminan componentes sin uso y sus rutas, se refactoriza búsqueda por continentes con constantes

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,25 +2,36 @@ import { Component } from '@angular/core';
 import { Observable, tap } from "rxjs";
 import { Country } from "../../interfaces/country";
 import { CountryService } from "../../services/country.service";
+import { KeyValue } from "@angular/common";
+
+enum Region {
+  Africa = 'Africa',
+  Americas = 'Americas',
+  Europe = 'Europe',
+  Asia = 'Asia',
+  Oceania = 'Oceania'
+}
 
 @Component({
   selector: 'countries-by-region-page',
-  templateUrl: './by-region-page.component.html',
-  styles: ``
+  templateUrl: './by-region-page.component.html'
 })
 export class ByRegionPageComponent {
   continent$!: Observable<Country[]>;
   isLoading: boolean = false;
+  selectedRegion?: string;
 
   constructor(private countryService: CountryService) {
   }
 
   searchByContinent(continent: string): void {
     this.isLoading = true;
+    this.selectedRegion = continent
     this.continent$ = this.countryService.searchByContinent(continent)
     .pipe(
       tap(() => this.isLoading = false)
     );
   }
 
+  protected readonly Region = Region;
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from "@angular/router";
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 
-
-
 @NgModule({
   declarations: [
-    HomePageComponent,
-    AboutPageComponent,
     SidebarComponent,
-    ContactPageComponent,
     SearchBoxComponent,
     SpinnerComponent
   ],
@@ -24,10 +16,7 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
     RouterModule
   ],
   exports: [
-    HomePageComponent,
-    AboutPageComponent,
     SidebarComponent,
-    ContactPageComponent,
     SearchBoxComponent,
     SpinnerComponent
   ]
